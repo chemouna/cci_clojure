b4e489fac87de2ce73147028bc4f76a41b77da30
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-01-29 23:59:11 +0100

    Solution #2 for 2.1

--- src/cci_clojure/chapter2/q1_remove_dupes.clj
+++ src/cci_clojure/chapter2/q1_remove_dupes.clj
@@ -1,9 +1,17 @@
 (ns cci-clojure.chapter2.q1-remove-dupes
   (:require [clojure.test :refer :all]))
 
+;; O(n^2)
 (defn de-dupe
   [s]
   (reduce (fn [res c] (if (some #{c} res) res (str res c))) "" s))
+
+(defn remove-dupes
+  [l]
+  (reduce (fn [deduped node]
+            (conj (remove #(= node %) deduped) node))
+  '()
+  (reverse l)))
 
 (deftest a-test
   (testing
@@ -12,3 +20,5 @@
     (is (= (de-dupe "FOLLOWUP") "FOLWUP"))))
 
 (run-tests)
+
+(remove-dupes "FOLLOWUP")
