2f55a588ef4c2ab99f736685d0c2853d1dc6266b
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-01-29 16:51:52 +0100

    fix small bug and add test for #9.

--- src/cci_clojure/chapter1/q9_is_rotation.clj
+++ src/cci_clojure/chapter1/q9_is_rotation.clj
@@ -1,5 +1,15 @@
-(ns cci-clojure.chapter1.q9-is-rotation)
+(ns cci-clojure.chapter1.q9-is-rotation
+  (:require [clojure.test :refer :all]))
 
-(defn isRotation
+(defn rotation?
   [s1 s2]
-  (.contains (str s1 s1) s2))
+  (and (= (count s1) (count s2))
+       (.contains (str s1 s1) s2)))
+
+(deftest a-test
+  (testing
+    (is (= (rotation? "abc" "ab") false))
+    (is (= (rotation? "abc" "abc") true))
+    (is (= (rotation? "waterbottle" "erbottlewat") true))))
+
+(run-tests)
